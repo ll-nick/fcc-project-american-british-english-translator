5580e8d60a4e84e9c057c297b2ad9e09a2e2aed3
ll-nick <github@le-large.net>
2023-08-11 00:10:18 +0200

    implement routes

--- routes/api.js
+++ routes/api.js
@@ -3,11 +3,22 @@
 const Translator = require('../components/translator.js');
 
 module.exports = function (app) {
-  
+
   const translator = new Translator();
 
   app.route('/api/translate')
     .post((req, res) => {
-      
+      try {
+        const text = req.body.text;
+        const locale = req.body.locale;
+        const translation = translator.translate(text, locale);
+
+        res.json({
+          text: text,
+          translation: translation
+        })
+      } catch (err) {
+        res.json({ error: err.message });
+      }
     });
 };
