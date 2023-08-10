78a5dfc23f1b892b36122af496784cc6002c69b0
ll-nick <github@le-large.net>
2023-08-10 23:34:37 +0200

    implement functional tests

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -6,7 +6,120 @@ const server = require('../server.js');
 chai.use(chaiHttp);
 
 let Translator = require('../components/translator.js');
+let translator = new Translator();
 
 suite('Functional Tests', () => {
+  test('valid request', function (done) {
+    const text = 'Mangoes are my favorite fruit.';
+    const translation = `Mangoes are my ${translator.highlight('favourite')} fruit.`
+    chai
+      .request(server)
+      .keepOpen()
+      .post('/api/translate')
+      .send({
+        text: text,
+        locale: 'american-to-british'
+      })
+      .end(function (err, res) {
+        assert.equal(res.status, 200);
+        assert.equal(res.type, 'application/json');
+        assert.equal(res.body.text, text);
+        assert.equal(res.body.translation, translation);
+        done();
+      });
+  });
 
+  test('invalid locale', function (done) {
+    const text = 'Mangoes are my favorite fruit.';
+
+    chai
+      .request(server)
+      .keepOpen()
+      .post('/api/translate')
+      .send({
+        text: text,
+        locale: 'american-to-german'
+      })
+      .end(function (err, res) {
+        assert.equal(res.status, 200);
+        assert.equal(res.type, 'application/json');
+        assert.equal(res.body.text, text);
+        assert.equal(res.body.error, 'Invalid value for locale field');
+        done();
+      });
+  });
+
+  test('missing text', function (done) {
+    const text = 'Mangoes are my favorite fruit.';
+
+    chai
+      .request(server)
+      .keepOpen()
+      .post('/api/translate')
+      .send({
+        locale: 'american-to-british'
+      })
+      .end(function (err, res) {
+        assert.equal(res.status, 200);
+        assert.equal(res.type, 'application/json');
+        assert.equal(res.body.text, text);
+        assert.equal(res.body.error, 'Required field(s) missing');
+        done();
+      });
+  });
+
+  test('missing locale', function (done) {
+    chai
+      .request(server)
+      .keepOpen()
+      .post('/api/translate')
+      .send({
+        text: text,
+      })
+      .end(function (err, res) {
+        assert.equal(res.status, 200);
+        assert.equal(res.type, 'application/json');
+        assert.equal(res.body.text, text);
+        assert.equal(res.body.error, 'Required field(s) missing');
+        done();
+      });
+  });
+
+  test('empty text', function (done) {
+    chai
+      .request(server)
+      .keepOpen()
+      .post('/api/translate')
+      .send({
+        text: '',
+        locale: 'american-to-british'
+      })
+      .end(function (err, res) {
+        assert.equal(res.status, 200);
+        assert.equal(res.type, 'application/json');
+        assert.equal(res.body.text, text);
+        assert.equal(res.body.error, 'No text to translate');
+        done();
+      });
+  });
+
+  test('no translation required', function (done) {
+    const text = 'Mangoes are my favourite fruit.';
+
+    chai
+      .request(server)
+      .keepOpen()
+      .post('/api/translate')
+      .send({
+        text: text,
+        locale: 'american-to-british'
+      })
+      .end(function (err, res) {
+        assert.equal(res.status, 200);
+        assert.equal(res.type, 'application/json');
+        assert.equal(res.body.text, text);
+        assert.equal(res.body.translation, 'Everything looks good to me!');
+        done();
+      });
+  });
 });
